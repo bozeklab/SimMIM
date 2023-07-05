cef51afacca969ec33a3883c2e58f6d7af4ea0ec
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-07-05 23:39:45 +0200

    add positional embedding

--- util/pos_embedding.py
+++ util/pos_embedding.py
@@ -9,7 +9,6 @@ import torch.nn as nn
 import torch
 from typing import Tuple
 from torch import Tensor
-import numpy as np
 
 
 class PositionalEmbedding(nn.Module):
@@ -125,10 +124,10 @@ class PositionalEmbedding(nn.Module):
 
     def forward(self, random_crop, grid_size, cls_token=False) -> Tuple[Tensor, Tensor]:
         #grid1, grid2 = self.calculate_grid(random_crop, grid_size)
-        self.grid2 = PositionalEmbedding.encode_relative_position(self.grid2, random_crop, grid_size)
+        grid2 = PositionalEmbedding.encode_relative_position(self.grid2, random_crop, grid_size)
 
         pos_embed1 = PositionalEmbedding.get_2d_sincos_pos_embed_from_grid(self.embed_dim, self.grid1)
-        pos_embed2 = PositionalEmbedding.get_2d_sincos_pos_embed_from_grid(self.embed_dim, self.grid2)
+        pos_embed2 = PositionalEmbedding.get_2d_sincos_pos_embed_from_grid(self.embed_dim, grid2)
 
         pos_embed1 = pos_embed1.float()
         pos_embed2 = pos_embed2.float()
