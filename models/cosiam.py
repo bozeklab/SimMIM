437edcece6662eb645f7dd46ba73b2a6677f91b3
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-07-07 10:29:00 +0200

    config fix

--- models/cosiam.py
+++ models/cosiam.py
@@ -149,7 +149,8 @@ class COSiam(nn.Module):
             z1m = self.momentum_encoder(x1)
             z2m = self.momentum_encoder(x2)
 
-        return z1, z2, z1m, z2m
+        return ya1, ya2, z1m, z2m
+        #return z1, z2, z1m, z2m
 
     @torch.jit.ignore
     def no_weight_decay(self):
