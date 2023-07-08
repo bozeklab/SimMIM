bf7bc4474dbd38b806a763c2a8382bbb64676dab
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-07-08 21:45:32 +0200

    debug

--- util/pos_embedding.py
+++ util/pos_embedding.py
@@ -134,9 +134,7 @@ class PositionalEmbedding(nn.Module):
 
 
         pos_embed1 = PositionalEmbedding.get_2d_sincos_pos_embed_from_grid(self.embed_dim, grid1)
-        pos_embed2 = PositionalEmbedding.get_2d_sincos_pos_embed_from_grid(self.embed_dim, grid2)
-        print(grid1.shape)
-        print(grid2.shape)
+        pos_embed2 = PositionalEmbedding.get_2d_sincos_pos_embed_from_grid(self.embed_dim, grid2, debug=True)
 
         pos_embed1 = pos_embed1.float()
         pos_embed2 = pos_embed2.float()
