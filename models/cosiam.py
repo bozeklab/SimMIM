f5b5e5d6f69f56b1c6389aecdf3398044d439608
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-07-08 20:28:09 +0200

    debug

--- models/cosiam.py
+++ models/cosiam.py
@@ -105,7 +105,7 @@ class VisionTransformerDecoder(VisionTransformer):
 
         p_a, p_b = self.pos_embed(random_crop, L ** 2)
         print('!!!')
-        print(p_b)
+        print(p_a)
 
         wpos = mask.flatten(1).unsqueeze(-1).type_as(p_a)
         p = p_a * (1 - wpos) + p_b * wpos
