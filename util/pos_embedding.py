dc5c651012c23115fd47c9c3d30d4de55e9c269a
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-07-08 21:59:13 +0200

    debug

--- util/pos_embedding.py
+++ util/pos_embedding.py
@@ -134,9 +134,12 @@ class PositionalEmbedding(nn.Module):
         grid1 = self.grid1.unsqueeze(0).repeat(batch_size, 1, 1, 1)
         grid2 = PositionalEmbedding.encode_relative_position(grid2, random_crop, grid_size)
 
+        print(grid1)
+        print('!!!')
+        print(grid2)
 
         pos_embed1 = PositionalEmbedding.get_2d_sincos_pos_embed_from_grid(self.embed_dim, grid1)
-        pos_embed2 = PositionalEmbedding.get_2d_sincos_pos_embed_from_grid(self.embed_dim, grid2, debug=True)
+        pos_embed2 = PositionalEmbedding.get_2d_sincos_pos_embed_from_grid(self.embed_dim, grid2)
 
         pos_embed1 = pos_embed1.float()
         pos_embed2 = pos_embed2.float()
